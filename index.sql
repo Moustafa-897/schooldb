820cafda087bb67263fba70cbd6523dfe60f7853
moustafa <alsayedmoustafa101997@gmail.com>
2025-12-10 21:25:26 +0200

    create table

--- index.sql
+++ index.sql
@@ -1,2 +1,6 @@
 -- create database school_db
 CREATE DATABASE school_db;
+
+-- create table
+CREATE TABLE students_table( student_id int, first_name varchar(50), last_name varchar(50), age int, grade varchar(5), city varchar(50) );
+
